8cdfbf45c5563c9bf56f42e600d2bdb3778e3c9f
etecjga <midori.kato@etec.sp.gov.br>
2025-09-26 13:37:41 -0300

    arrumanod ver senha

--- autentificacao/autentica.js
+++ autentificacao/autentica.js
@@ -10,10 +10,23 @@ pessoaBtn.addEventListener('click', () => {
     container.classList.remove("active");
 });
 
-/*Ver senha*/
+/*Ver senha login*/
 let senhaV = document.querySelector('#verSenha')
 
 senhaV.addEventListener('click', ()=>{
+    let inputSenha = document.querySelector('#senha')
+
+    if(inputSenha.getAttribute('type') == 'password'){
+        inputSenha.setAttribute('type', 'text')
+    }else{
+        inputSenha.setAttribute('type', 'password')
+    }
+});
+
+/*Ver senha cadastro*/
+let senhaVer = document.querySelector('#SenhaVer')
+
+senhaVer.addEventListener('click', ()=>{
     let inputSenha = document.querySelector('#senha')
 
     if(inputSenha.getAttribute('type') == 'password'){
